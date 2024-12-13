741e2e6c37840a8b35c361a14519aa0ce2e9f6cd
Jonathan <131307491+jonathanzietsman@users.noreply.github.com>
2024-12-13 18:01:44 +0200

    fix img links

---
+++ Chapter 7/robot.js
@@ -0,0 +1,29 @@
+// 3: Page 106
+document.getElementById("lefteye").style.backgroundColor = "purple";
+
+// 5: Page 107
+document.getElementById("head").style.transform = "rotate(15deg)";
+
+/* Experimenting with Douglas: Page 108 */
+// Put a 2‐pixel‐wide, solid black border around his body.
+document.getElementById("body").style.border = "2px solid black";
+// Round the corners of his mouth
+document.getElementById("mouth").style.borderRadius = "50px";
+// Put yellow dots around his right eye.
+document.getElementById("righteye").style.border = "4px dotted yellow";
+// Change his left arm’s color
+document.getElementById("leftarm").style.backgroundColor = "grey";
+// Change the text color.
+document.getElementById("body").style.color = "green";
+// Give Douglas hair.
+document.getElementById("head").style.borderTop = "10px black solid";
+
+/* ✔ Page 109 */
+// Tilt Douglas’s head to the other side.
+document.getElementById("head").style.transform = "rotate(-15deg)";
+// Make Douglas’s nose round.
+document.getElementById("nose").style.borderRadius = "100px";
+// Make Douglas’s right arm green.
+document.getElementById("rightarm").style.backgroundColor = "green";
+// Make Douglas’s lips pink.
+document.getElementById("mouth").style.border = "10px solid pink"
